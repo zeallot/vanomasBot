932d1c6588ee0b22ba73d4db5649c77a7ffffab8
vkaboskin <vkaboskin@aspirity.com>
2023-01-19 21:05:51 +0700

    init project

---
+++ index.js
@@ -0,0 +1,53 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { telegramToken } = require('./config');
+
+const bot = new TelegramBot(telegramToken, {polling: true});
+
+const results = [
+  {
+    id: 1,
+    type: 'voice',
+    title: 'Сосите биоскот',
+    voice_url: "AwACAgIAAxkBAAMHY8lJ9h3FTCgFw2F25vi5Yy6olA0AAmgkAAIFfEBKKV7p2vZ4btstBA"
+  },
+  {
+    id: 2,
+    type: 'voice',
+    title: 'Сука унтер ебучий',
+    voice_url: "AwACAgIAAxkBAAMGY8lJzhb0auUwJ-Io8yfS-L_sa8cAAm0kAAIFfEBK7dNprIN42tMtBA"
+  },
+  {
+    id: 3,
+    type: 'voice',
+    title: 'Разумизм гугли',
+    voice_url: "AwACAgIAAxkBAAMFY8lJuna-rCNtvfdPfDHT2a9-dLgAAnEkAAIFfEBKCyH2xLsgRoctBA"
+  },
+  {
+    id: 4,
+    type: 'voice',
+    title: 'Хуй соси',
+    voice_url: "AwACAgIAAxkBAAMEY8lJmhRN3Db3EhEMSjxYdxPXy_wAAoUkAAIFfEBKgqDHDwjLUJktBA"
+  },
+  {
+    id: 5,
+    type: 'voice',
+    title: 'Об унитаз',
+    voice_url: "AwACAgIAAxkBAAMDY8lJhSX2W-kTA3PBM7-_yBSecqcAAqEkAAIFfEBKyTFBs2Nx3p8tBA"
+  },
+  {
+    id: 6,
+    type: 'voice',
+    title: 'Глотку выгрузу',
+    voice_url: "AwACAgIAAxkBAAMCY8lJY-wcLhtymMxaRqQGzDJfvcUAAgssAAIFfEhKnoKOmtTMNsQtBA"
+  }
+];
+
+
+bot.on('inline_query',  async msg => {
+  await bot.answerInlineQuery(msg.id,
+    results.filter((v) => !!v.title.toLowerCase().includes(msg.query.toLowerCase())))
+})
+
+bot.on('message',  async msg => {
+  await bot.sendVoice(msg.chat.id, results[Math.floor(Math.random() * results.length)].voice_url)
+})
